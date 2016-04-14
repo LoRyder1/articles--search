b45995cf62e8b25bd67eab5442c5337bdfb4a51f
Lovinder Pnag <lspnag@gmail.com>
2016-04-13 23:40:44 -0400

    working elastic search

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,8 @@
 require 'elasticsearch/model'
 
 class Article < ActiveRecord::Base
-  include Elasticsearch::model
+  include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
 end
 
-Article.import
+# Article.import
